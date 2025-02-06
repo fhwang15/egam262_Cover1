4fecd4ca1c87ae8631fc4dd07957240c6d76af75
fhwang15 <fhwang@inside.artcenter.edu>
2025-02-06 22:24:30 +0900

    initial push
    - initial push
    - with background and partial trackdrawer done

---
+++ Assets/Scripts/TrackDrawer.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackDrawer : MonoBehaviour
+{
+
+    public LineRenderer lineRenderer;
+    public float minWaypointDistance = 0.5f; //Distance between the waypoints
+
+
+    List<Vector3> wayPoints = new List<Vector3>();
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
